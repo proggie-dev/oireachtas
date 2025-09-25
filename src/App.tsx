af4cb33b49f529408d98e81e379e89a97db1c097
proggie-dev <pandurevic.o@gmail.com>
2025-09-25 10:13:42 +0200

    Edit MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import AppContainer from './ui/index';
 import './styles/App.scss';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 
-const App = () => <AppContainer />;
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Cormorant Garamond', serif",
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <AppContainer />
+  </ThemeProvider>
+);
 
 export default App;
